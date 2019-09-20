1d7acb241d579574d5d5ab922354241950c6f755
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-09-20 12:23:48 +0200

    Remove message.ts

--- src/app/pages/login/message.ts
+++
@@ -1,5 +0,0 @@
-export enum Message {
-    ALL_FIELDS_ARE_EMPTY = 'Please fill out the given fields',
-    MISSING_EMAIL = 'Email address is missing',
-    MISSING_PASSWORD = 'Password is missing'
-}
