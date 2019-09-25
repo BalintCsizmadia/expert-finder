0923d4875440840dff5533453a3ac6141f720f4a
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-09-25 09:32:03 +0200

    Add basic API calls

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { Customer } from '../models/customer';
+import { RegistrationDetails } from '../models/login-details';
+import { CustomerRegistrationDetails } from '../models/customer-registration-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // get all users
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('/api/users');
+  }
+
+  // get all customers
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>('/api/customers');
+  }
+
+  // get user or customer by id
+  getUserById(id: number): Observable<User | Customer> {
+    return this.http.post<User | Customer>('/api/user/{id}', { id });
+  }
+
+  addCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>('/api/user/add', { customer });
+  }
+
+  userRegister(registerDetails: RegistrationDetails): Observable<void> {
+    return this.http.post<void>('/api/user/register', { registerDetails });
+  }
+
+  customerRegister(registerDetails: CustomerRegistrationDetails): Observable<void> {
+    return this.http.post<void>('/api/customer/register', { registerDetails });
+  }
 }
