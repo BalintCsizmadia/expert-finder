ea345c2f7b12b43435803b98b770056c4480c22a
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-09-25 09:25:16 +0200

    Add route for registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // TODO page login
+  { path: '', pathMatch: 'full', loadChildren: './pages/login/login.module#LoginPageModule' },
   {
     path: 'page',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: '', pathMatch: 'full', loadChildren: './pages/login/login.module#LoginPageModule' }
+  { path: 'registration', loadChildren: './pages/registration/registration.module#RegistrationPageModule' }
 ];
+
+// {
+//   path: '',
+//   loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+// }
+// ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
