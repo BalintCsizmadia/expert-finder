21d480ca0ce9ceac22f6b4e75c1cc7640dd14895
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-09-30 14:38:55 +0200

    Add customer registration

--- backend/src/main/java/com/own/expertfinder/controller/CustomerController.java
+++ backend/src/main/java/com/own/expertfinder/controller/CustomerController.java
@@ -1,4 +1,42 @@
 package com.own.expertfinder.controller;
 
+import com.own.expertfinder.exception.UserAlreadyExistsException;
+import com.own.expertfinder.model.Customer;
+import com.own.expertfinder.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
-}
+
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping(path = "/customers",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Customer> getUsers() {
+        return customerService.getAll();
+    }
+
+    @RequestMapping(path = "/customer/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Customer getUserById(@PathVariable("id") Integer id) {
+        return customerService.getOne(id);
+    }
+
+    @RequestMapping(path = "/customer/register",
+            method = RequestMethod.POST
+    )
+    public int add(@RequestBody Customer customer) throws UserAlreadyExistsException {
+        try {
+            return customerService.add(customer);
+        } catch (UserAlreadyExistsException e) {
+            e.printStackTrace();
+            throw new UserAlreadyExistsException();
+        }
+    }}
--- backend/src/main/java/com/own/expertfinder/service/CustomerService.java
+++ backend/src/main/java/com/own/expertfinder/service/CustomerService.java
@@ -1,12 +1,19 @@
 package com.own.expertfinder.service;
 
+import com.own.expertfinder.exception.UserAlreadyExistsException;
 import com.own.expertfinder.model.Customer;
 import com.own.expertfinder.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class CustomerService {
@@ -21,14 +28,47 @@ public class CustomerService {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
+
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
 
     public Customer getOne(Integer id) {
         return customerRepository.getOne(id);
     }
 
-    public Customer getUserByName(String username) {
+    public Customer getCustomerByName(String username) {
         return customerRepository.findByUsername(username);
+    }
+
+    public int add(Customer customer) throws UserAlreadyExistsException {
+        if (!isUsernameExists(customer.getUsername())) {
+            // return customerRepository.save(customer);
+            // TODO Refactor / Change
+            return jdbcTemplate.update(
+                    "INSERT INTO customers " +
+                            "(username, password, first_name, last_name, phone_number, profession, position, registration_date) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, to_json(?::json), NOW())",
+                    customer.getUsername(),
+                    customer.getPassword(),
+                    customer.getFirstName(),
+                    customer.getLastName(),
+                    customer.getPhoneNumber(),
+                    customer.getProfession(),
+                    customer.getPosition()
+            );
+        } else {
+            throw new UserAlreadyExistsException("Repeated registration attempt. User already exists.");
+        }
+    }
+
+    private boolean isUsernameExists(String username) {
+        Customer customer = getCustomerByName(username);
+        return customer != null;
     }
 
 }
