af1a29ee46db69826d5740115d459c2e43359f33
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-10-01 14:34:24 +0200

    Add classes for 'visitors'

---
+++ backend/src/main/java/com/own/expertfinder/repository/VisitorRepository.java
@@ -0,0 +1,19 @@
+package com.own.expertfinder.repository;
+import com.own.expertfinder.model.Visitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
+
+    List<Visitor> findAll();
+
+    Visitor findByEmail(String email);
+
+    Visitor findByUserId(Integer id);
+
+    @Override
+    <S extends Visitor> S save(S s);
+}
