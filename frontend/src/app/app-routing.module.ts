e7a5c43447e6d2a4ed1cc4cf3f180483dad47333
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-10-09 12:37:28 +0200

    TEMP loginGuard hiding

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginGuardService } from './services/login-guard.service';
 
 const routes: Routes = [
   // TODO page login
-  { path: '', pathMatch: 'full', loadChildren: './pages/login/login.module#LoginPageModule' },
   {
-    path: 'page',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    path: '',
+    pathMatch: 'full',
+    loadChildren: './pages/login/login.module#LoginPageModule',
+    // canActivate: [LoginGuardService]
   },
-  { path: 'registration', loadChildren: './pages/registration/registration.module#RegistrationPageModule' }
+  {
+    path: 'registration',
+    loadChildren: './pages/registration/registration.module#RegistrationPageModule'
+  },
+  {
+    path: 'visitor',
+    loadChildren: () => import('./pages/visitor/tabs/tabs.module').then(m => m.TabsPageModule),
+    // canActivate: [LoginGuardService]
+  },
+  {
+    path: 'customer',
+    loadChildren: () => import('./pages/customer/tabs/tabs.module').then(m => m.TabsPageModule),
+    // canActivate: [LoginGuardService]
+  }
 ];
 
 // {
