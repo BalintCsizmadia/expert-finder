96ed3135691509af95fa732bec116d60ef59da31
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-10-17 11:02:02 +0200

    Refactor

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -4,36 +4,36 @@ DROP TABLE IF EXISTS visitors;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	username TEXT NOT NULL UNIQUE,
-	password TEXT NOT NULL,
-	enabled BOOLEAN,
-	registration_date DATE
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    enabled BOOLEAN,
+    registration_date DATE
 );
 
 CREATE TABLE visitors (
-	id SERIAL PRIMARY KEY,
-	user_id INTEGER NOT NULL,
-	email TEXT NOT NULL UNIQUE,
-	registration_date DATE,
-	FOREIGN KEY(user_id) REFERENCES users(id)
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    registration_date DATE,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE customers (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  email TEXT NOT NULL UNIQUE,
-  first_name TEXT,
-  last_name TEXT,
-  phone_number TEXT,
-  profession TEXT,
-  is_active BOOLEAN,
-  position JSON,
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    first_name TEXT,
+    last_name TEXT,
+    phone_number TEXT,
+    profession TEXT,
+    is_active BOOLEAN,
+    position JSON,
   -- default_position JSON ?
-  status INTEGER,
-  available_from TIMESTAMP,
-  registration_date DATE,
-  FOREIGN KEY(user_id) REFERENCES users(id)
+    status INTEGER,
+    available_from TIMESTAMP,
+    registration_date DATE,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE authorities (
