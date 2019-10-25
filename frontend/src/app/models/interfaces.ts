38a48348f91b5af156b310d7109ad44731675629
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-10-25 10:29:49 +0200

    Add Profession interface

--- frontend/src/app/models/interfaces.ts
+++ frontend/src/app/models/interfaces.ts
@@ -2,27 +2,32 @@ import { Status } from './enums';
 import { User } from './user';
 
 export interface Position {
-    latitude: number;
-    longitude: number;
-    timestamp: Date;
-  }
+  latitude: number;
+  longitude: number;
+  timestamp: Date;
+}
 
 export interface LoggedInUser {
-    // is user a 'visitor'
-    id: number;
-    username: string;
-    authorities: string[];
-    // if user a 'customer'
-    firstName?: string;
-    lastName?: string;
-    phoneNumber?: string;
-    profession?: string;
-    // when GPS data are available. Esle default location displays
-    isActive?: boolean;
-    // '_position' arrives from database as a 'string'
-    // then it will be a 'Position' object
-    position?: Position | string;
-    status?: Status | null;
-    availableFrom?: number; // time in milliseconds
-    user?: User;
+  // is user a 'visitor'
+  id: number;
+  username: string;
+  authorities: string[];
+  // if user a 'customer'
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  profession?: string;
+  // when GPS data are available. Esle default location displays
+  isActive?: boolean;
+  // '_position' arrives from database as a 'string'
+  // then it will be a 'Position' object
+  position?: Position | string;
+  status?: Status | null;
+  availableFrom?: number; // time in milliseconds
+  user?: User;
+}
+
+export interface Profession {
+  id: number;
+  name: string;
 }
