772fa35bb8d654be80da440c40c84b27fb67b016
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-10-25 10:32:29 +0200

    Handle search by profession

--- frontend/src/app/pages/visitor/tab1/tab1.page.ts
+++ frontend/src/app/pages/visitor/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { NavigationExtras } from '@angular/router';
+import { ProfessionService } from 'src/app/services/profession.service';
+import { Profession } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,36 +11,45 @@ import { NavController } from '@ionic/angular';
 })
 export class Tab1Page implements OnInit, AfterContentInit {
 
-  constructor(private navCtrl: NavController) {
-    console.log('costrukt');
-   }
+  professions: Profession[] = [];
+  selectedProfession: number; // professionId
+
+  constructor(private professionService: ProfessionService, private navCtrl: NavController) { }
 
   ngOnInit() {
     const searchbar = document.getElementById('search');
     searchbar.addEventListener('ionInput', this.handleSearchInputChange);
-    console.log(this.navCtrl);
+    this.professionService.getAllProfessions().then((professions: Profession[]) => {
+      this.professions = professions;
+    });
   }
 
   handleSearchInputChange = (event: any) => {
-    // TODO implement well
-    const items = Array.from(document.querySelector('ion-list').children);
+    const items = Array.from(document.getElementById('inner-product-list').children);
     const query = event.target.value.toLowerCase();
     requestAnimationFrame(() => {
       items.forEach((item: any) => {
-        console.log(item.children);
         const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
         item.style.display = shouldShow ? 'block' : 'none';
       });
     });
+  }
 
-    // console.log(event.target.value);
-    if (event.target.value === 'map') {
-      this.navigateToMap();
-    }
+  radioGroupChange = (event: any) => {
+    this.selectedProfession = +event.target.value;
+  }
+
+  onSearchClick = () => {
+    this.navigateToMap();
   }
 
   navigateToMap = () => {
-    this.navCtrl.navigateForward('/visitor/tabs/tab2');
+    const params: NavigationExtras = {
+      queryParams: {
+        professionId: +this.selectedProfession
+      }
+    };
+    this.navCtrl.navigateForward('/visitor/tabs/tab2', params);
   }
 
 
